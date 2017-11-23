6f994a750697f415648e78109dabf1849327c623
Rosa Mohammadi <rosamohammadi@hotmail.com>
2017-11-22 22:12:05 -0800

    removed heat index code, included error checking, unit tests added

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,52 +3,44 @@
 #include "DHT.h"
 #include "TH.h"
 
-#define ERROR_TEMP "DHT Temperature sensor"
-#define ERROR_HUMIDITY "DHT Humidity sensor"
-#define ERROR_HEATINDEX "DHT Heat Index calculator"
-
 /*
- * Purpose: initializes the DHT sensor and polls the sensor till it obtains
- * temperature and humidity data. This needs to be in a .ino file
- * (not c file) as DHT library code is in c++.
- * Output: the TH struct containing all DHT sensor data
- */
- struct TH init_dht(void) {
+* Purpose: initializes the DHT sensor and polls the sensor till it obtains
+* temperature and humidity data. This needs to be in a .ino file
+* (not c file) as DHT library code is in c++.
+* Output: the TH struct containing all DHT sensor data
+*/
+struct TH init_dht(void) {
 
-   struct TH t_h = {-1000, -1000, -1000};
-   float h;
-   float t;
-   float hi;
+  struct TH t_h = {-1000, -1000};
+  float h;
+  float t;
 
-   // initialize DHT sensor
-   DHT dht(PIN_DHT, DHTTYPE);
-   dht.begin();
+  // initialize DHT sensor
+  DHT dht(PIN_DHT, DHTTYPE);
+  dht.begin();
 
-   int attempt = 0; // track number of poll attempts
+  int attempt = 0; // track number of poll attempts
 
-   // try to poll data for 20 secs max
-   do {
-     // can only poll every 2 seconds
-     delay(2000);
-     // reading temperature or humidity takes about 250 ms
-     h = dht.readHumidity();
-     // read temperature as Celsius (default)
-     t = dht.readTemperature();
-     attempt++;
-   } while((isnan(h) || isnan(t)) & attempt < 10);
+  // try to poll data for 20 secs max
+  do {
+    // can only poll every 2 seconds
+    delay(2000);
+    // reading temperature or humidity takes about 250 ms
+    h = dht.readHumidity();
+    // read temperature as Celsius (default)
+    t = dht.readTemperature();
+    attempt++;
+  } while((isnan(h) || isnan(t)) & attempt < 10);
 
-   // compute heat index in Celsius (isFahreheit = false)
-   //hi = dht.computeHeatIndex(t, h, false);
+  if (!isnan(t)) {
+    t_h.t = t;
+  }
+  if (!isnan(h)) {
+    t_h.h = h;
+  }
 
-   if (!isnan(t)) {
-     t_h.t = t;
-   }
-   if (!isnan(h)) {
-     t_h.h = h;
-   }
-
-   return t_h;
- }
+  return t_h;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -58,32 +50,23 @@ void setup() {
   /* DHT temperature and humidity code */
   struct TH t_h = init_dht();
 
-    error = FALSE;
-    float t = get_temp(t_h, &error);
-    if (error) {
-      Serial.print(ERROR_TEMP);
-    }
-    error = FALSE;
-    float h = get_humidity(t_h, &error);
-    if (error) {
-      Serial.print(ERROR_HUMIDITY);
-    }
-    /*
-    error = FALSE;
-    float hi = get_heatindex(t_h, &error);
-    if (error) {
-      Serial.print(ERROR_HEATINDEX);
-    }*/
+  error = FALSE;
+  float t = get_temp(t_h, &error);
+  if (error) {
+    Serial.print(ERROR_TEMP);
+  }
+  error = FALSE;
+  float h = get_humidity(t_h, &error);
+  if (error) {
+    Serial.print(ERROR_HUMIDITY);
+  }
 
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" \t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    /*Serial.print("Heat index: ");
-    Serial.print(hi);
-    Serial.print(" *C ");*/
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" \t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
 
 }
 
--- libraries/DHT/TH.h
+++ libraries/DHT/TH.h
@@ -8,22 +8,7 @@
 struct TH {
   volatile float h; // humidity
   volatile float t; // temperature
-  volatile float hi; // heat index
 };
-
-/*
- * Purpose: Determines whether temp and humidity data
- * have been obtained.
- * Output: true or false
- */
- /*
-int is_dht_ready(struct TH t_h, int *error) {
-  if (t_h.has_data == FALSE || t_h.has_data == TRUE) {
-    return t_h.has_data;
-  }
-  *error = TRUE;
-  return 0;
-}*/
 
 /*
  * Purpose: Gets the temperature data
@@ -34,7 +19,7 @@ float get_temp(struct TH t_h, int *error) {
     return t_h.t;
   }
   *error = TRUE;
-  return 0;
+  return -1000;
 }
 
 /*
@@ -46,19 +31,7 @@ float get_humidity(struct TH t_h, int *error) {
     return t_h.h;
   }
   *error = TRUE;
-  return 0;
-}
-
-/*
- * Purpose: Calculates and gets the heat index
- * Output: heat index in Celsius
- */
-float get_heatindex(struct TH t_h, int *error) {
-  if (t_h.hi != -1000) {
-    return t_h.hi;
-  }
-  *error = TRUE;
-  return 0;
+  return -1000;
 }
 
 #endif
