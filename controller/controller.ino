caa7d17c17ec156a2818fb2acc9e73fa11435bb1
Joshua Tan <joshtantan@alumni.ubc.ca>
2017-11-27 01:55:13 -0800

    Updated get_co2_voltage and get_co2_concentration
    Changed calling procedure and variable passing

--- controller/controller.ino
+++ controller/controller.ino
@@ -6,11 +6,16 @@ void setup() {
   Serial.begin(9600);
   // set default reference voltage
   analogReference(DEFAULT);
+  float co2_volt, co2_conc;
   // set error variable to default
-  error = FALSE;
-  float co2_conc = get_co2_concentration(get_co2_voltage(&error));
+  error = FALSE; 
+  co2_volt = get_co2_voltage(&error);
   if (error) {
-    Serial.print(ERROR_CO2);
+    Serial.print(ERROR_GCO2V);
+  }
+  co2_conc = get_co2_concentration(co2_volt, &error);
+  if (error) {
+    Serial.print(ERROR_GCO2C);
   }
   Serial.print("CO2 Concentration: ");
   Serial.print(co2_conc);
