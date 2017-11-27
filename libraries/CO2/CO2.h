b55b803b6bc5bd9e950200c1917133d8fa76b435
David <ion884@gmail.com>
2017-11-26 23:42:33 -0800

    Update CO2.h

--- libraries/CO2/CO2.h
+++ libraries/CO2/CO2.h
@@ -4,12 +4,44 @@
 #define TRUE 1
 #define FALSE 0
 
-float get_concentration(float co2, int *error) {
-  if (co2 != -1000) {
-    return co2;
+/*
+ * Purpose: Polls the Infrared CO2 Sensor till it obtains a sensor value and calculates
+ *          the CO2 concentration value.
+ * Output: A float contraining CO2 concentration in ppm or a respective error
+ */
+float get_concentration(int *error) {
+  float co2_conc = -1000; // default value used to detect null readings
+
+  int sensor_value;
+
+  int attempt = 0; // track number of poll attempts
+
+  // try to poll data for 1 sec max
+  do {
+    // can only poll every 100 milliseconds
+    delay(100);
+    // read voltage
+    sensor_value = analogRead(PIN_CO2);
+    attempt++;
+  } while(isnan(sensor_value) & attempt < 10);
+
+  // analog signal is converted to a voltage value
+  float voltage = sensor_value * (5000 / 1024.0);
+
+  if(voltage < 400) {
+    return co2_conc;
   }
-  *error = TRUE;
-  return -1000;
+  else {
+    int voltage_difference = voltage - 400;
+    co2_conc = voltage_difference * (50.0 / 16.0);
+  }
+
+  if (co2_conc != -1000) {
+    return co2_conc;
+  } else {
+    *error = TRUE;
+    return -1000;
+  }
 }
 
 #endif
