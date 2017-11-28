11b7cedc0b7dd664b4400df1c4db3dea3f2ef50b
Rosa Mohammadi <rosamohammadi@hotmail.com>
2017-11-28 13:50:00 -0800

    integrated co2 branch

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,100 +1,25 @@
+
 #include "constants.h"
-#include "DHT.h"
-#include "TH.h"
-#include "SDCard.h"
-
-/*
-* Purpose: initializes the DHT sensor and polls the sensor till it obtains
-* temperature and humidity data. This needs to be in a .ino file
-* (not c file) as DHT library code is in c++.
-* Output: the TH struct containing all DHT sensor data
-*/
-struct TH init_dht(void) {
-
-  /* to determine whether a temp and humidity reading was obtained, we
-   initialize them to -1000 since those are invalid values */
-  struct TH t_h = {-1000, -1000};
-  float h;
-  float t;
-
-  // initialize DHT sensor
-  DHT dht(PIN_DHT, DHTTYPE);
-  dht.begin();
-
-  int attempt = 0; // track number of poll attempts
-
-  // try to poll data for 20 secs max
-  do {
-    // can only poll every 2 seconds
-    delay(2000);
-    // reading temperature or humidity takes about 250 ms
-    h = dht.readHumidity();
-    // read temperature as Celsius (default)
-    t = dht.readTemperature();
-    attempt++;
-  } while((isnan(h) || isnan(t)) & attempt < 10);
-
-  if (!isnan(t)) {
-    t_h.t = t;
-  }
-  if (!isnan(h)) {
-    t_h.h = h;
-  }
-
-  return t_h;
-}
-
+#include "CO2.h"
+int error;
 void setup() {
   Serial.begin(9600);
-  // set default reference voltage (5V)
+  // set default reference voltage
   analogReference(DEFAULT);
-
-  int error;
-
-  /* DHT temperature and humidity code */
-  struct TH t_h = init_dht();
-
-  error = FALSE;
-  float t = get_temp(t_h, &error);
+  float co2_volt, co2_conc;
+  // set error variable to default
+  error = FALSE; 
+  co2_volt = get_co2_voltage(&error);
   if (error) {
-    Serial.print(ERROR_TEMP);
+    Serial.print(ERROR_GCO2V);
   }
-  error = FALSE;
-  float h = get_humidity(t_h, &error);
+  co2_conc = get_co2_concentration(co2_volt, &error);
   if (error) {
-    Serial.print(ERROR_HUMIDITY);
+    Serial.print(ERROR_GCO2C);
   }
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" \t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  
-  /* SD interfacing code */
-  error = FALSE;
-  struct SD_card sd = init_sd("data.txt", error); // TODO check error pointer 
-
-  // writes/reads to SD Card if initialized properly
-  if(!error) {
-    Serial.println("Begin writing to SD");
-    // TODO pass in sensor vals
-    /*
-    for(float i = 0; i < 10; ++i){
-      write_sd(sd, i, i, i, error);
-    }
-    
-    // reads from SD card
-    read_sd(sd, error);
-    */
-    SD.remove("data.txt"); // TODO create const filename
-    
-  } 
-  Serial.println("SD Finished");
-
+  Serial.print("CO2 Concentration: ");
+  Serial.print(co2_conc);
+  Serial.print(" ppm");
 }
-
 void loop() {
-
 }
---
+++ libraries/CO2/CO2.h
@@ -0,0 +1,67 @@
+#ifndef CO2_H
+#define CO2_H
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include "constants.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+/*
+ * Purpose: Polls the Infrared CO2 Sensor till it obtains a sensor value and calculates
+ *          the CO2 concentration value.
+ * Output: A float contraining CO2 concentration in ppm or a respective error
+ */
+float get_co2_voltage(int *error) {
+  int sensor_value;
+
+  int attempt = 0; // track number of poll attempts
+
+  // try to poll data for 1 sec max
+  do {
+    // can only poll every 100 milliseconds
+    delay(100);
+    // read voltage
+    sensor_value = analogRead(PIN_CO2);
+    attempt++;
+  } while(isnan(sensor_value) & attempt < 10);
+
+  // analog signal is converted to a voltage value
+  float voltage = sensor_value * (5000 / 1024.0);
+  
+  // check for invalid output
+  if (voltage < 400 || voltage > 2000){
+    *error = TRUE;
+    return voltage; 
+  }
+  
+  return voltage;
+}
+
+float get_co2_concentration(float voltage, int *error){
+  // check for invalid input
+  if (voltage < 400 || voltage > 2000){
+    *error = TRUE;
+    return voltage; 
+  }
+  
+  int voltage_difference = voltage - 400;
+  float co2_conc = voltage_difference * (50.0 / 16.0);
+  
+  //check for invalid output
+  if (co2_conc < 0 || co2_conc > 5000){
+    *error = TRUE;
+    return co2_conc; 
+  }
+
+  return co2_conc;
+}
+
+#endif
