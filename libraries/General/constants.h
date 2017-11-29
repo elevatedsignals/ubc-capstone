9fd46c491e7381f8d1d8ea460f5b5ab8f8e1b3ee
David <ion884@gmail.com>
2017-11-28 18:50:46 -0800

    Update constants.h

--- libraries/General/constants.h
+++ libraries/General/constants.h
@@ -4,12 +4,15 @@
 #define TRUE 1
 #define FALSE 0
 
+#define MODULE_ID 12345
 #define PIN_CO2 A0
 #define PIN_DHT 2
 #define PIN_SD 10
 #define PIN_SD_CHECK 9
 
 #define DHTTYPE DHT22
+
+#define TXT_FILE "data.txt"
 
 #define ERROR_TEMP "Error: DHT Temperature sensor"
 #define ERROR_HUMIDITY "Error: DHT Humidity sensor"
