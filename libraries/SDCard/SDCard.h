782fd6f3e19e513ac13c95e9393dc8d58c15ba02
David <ion884@gmail.com>
2017-11-28 18:51:31 -0800

    Update SDCard.h

--- libraries/SDCard/SDCard.h
+++ libraries/SDCard/SDCard.h
@@ -15,7 +15,7 @@ struct SD_card {
  * Purpose: Initialize the SD card and allows for reading and writing
  * Output: N/A
  */
- struct SD_card init_sd(String file_name, int &error) {
+ struct SD_card init_sd(String file_name, int *error) {
 
    struct SD_card sd;
    sd.file_name = file_name;
@@ -32,12 +32,12 @@ struct SD_card {
    }
    else if(!digitalRead(PIN_SD_CHECK)) {
      Serial.println(ERROR_NOSD);
-     error = TRUE;
+     *error = TRUE;
     }
 
    else {
      Serial.println(ERROR_INITSD);
-     error = TRUE;
+     *error = TRUE;
    }
 
    return sd;
@@ -47,7 +47,7 @@ struct SD_card {
  * Purpose: Write to SD file name and return whether or not write was successful
  * Output: true or false
  */
-int write_sd(struct SD_card sd, float temperature_data, float humidity_data, float CO2_data, int &error) {
+int write_sd(struct SD_card sd, float temperature_data, float humidity_data, float CO2_data, int *error) {
 
   // open file for writing
   sd.write_file = SD.open(sd.file_name, FILE_WRITE);
@@ -55,37 +55,41 @@ int write_sd(struct SD_card sd, float temperature_data, float humidity_data, flo
   // check that file opened properly
   if(sd.write_file) {
 
-	sd.write_file.print("{ \"temperature\":");
-	if(temperature_data == ERROR_VALUE) {
+    sd.write_file.print("{ \"id\":");
+    sd.write_file.print(MODULE_ID);
+    sd.write_file.print(",");
+
+  sd.write_file.print("\"temperature\":");
+  if(temperature_data == ERROR_VALUE) {
       sd.write_file.print("NaN");
-	}
-	else {
-	  sd.write_file.print(temperature_data);
-	}
-	sd.write_file.print(",");
+  }
+  else {
+    sd.write_file.print(temperature_data);
+  }
+  sd.write_file.print(",");
 
-	sd.write_file.print("\"humidity\":");
-	if(humidity_data == ERROR_VALUE) {
-	  sd.write_file.print("NaN");
-	}
-	else {
-	  sd.write_file.print(humidity_data);
-	}
-	sd.write_file.print(",");
+  sd.write_file.print("\"humidity\":");
+  if(humidity_data == ERROR_VALUE) {
+    sd.write_file.print("NaN");
+  }
+  else {
+    sd.write_file.print(humidity_data);
+  }
+  sd.write_file.print(",");
 
-	sd.write_file.print("\"CO2\"");
-	if(CO2_data == ERROR_VALUE) {
-	  sd.write_file.print("NaN");
-	}
-	else{
+  sd.write_file.print("\"CO2\":");
+  if(CO2_data == ERROR_VALUE) {
+    sd.write_file.print("NaN");
+  }
+  else{
       sd.write_file.print(CO2_data);
-	}
-	sd.write_file.print("\n");
+  }
+  sd.write_file.print(" }\n");
 
   }
   else {
-	// write unsuccessful
-	error = TRUE;
+  // write unsuccessful
+  *error = TRUE;
     return 0;
   }
 
@@ -98,7 +102,7 @@ int write_sd(struct SD_card sd, float temperature_data, float humidity_data, flo
  * Purpose: Read from SD file name and return whether or not read was successful
  * Output: true or false
  */
-int read_sd(struct SD_card sd, int &error){
+int read_sd(struct SD_card sd, int *error){
 
   //open file for reading
   sd.read_file = SD.open(sd.file_name, FILE_READ);
@@ -107,15 +111,15 @@ int read_sd(struct SD_card sd, int &error){
 
     // Read from file until there's nothing else in it
     while(sd.read_file.available()) {
-	  Serial.write(sd.read_file.read());
-	}
+    Serial.write(sd.read_file.read());
+  }
 
   }
   else {
     // read unsuccessful
-	error = TRUE;
-	sd.read_file.close();
-	return 0;
+  *error = TRUE;
+  sd.read_file.close();
+  return 0;
   }
 
   sd.read_file.close();
