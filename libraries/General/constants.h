19e19cb78fde69bfcd14f5abdc5ad2c1d316a7b1
49V <moshelawlor@gmail.com>
2018-02-12 18:11:53 -0800

    Moves timestamp code into header file ("timestamp.h")

--- libraries/General/constants.h
+++ libraries/General/constants.h
@@ -24,21 +24,4 @@
 #define ERROR_INITSD "Error: Failed to initialize SD Card"
 #define ERROR_INITUDP "Error: Failed to configure Ethernet using DHCP"
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-
-unsigned int localPort = 8888;       // local port to listen for UDP packets
-
-char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
-
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-EthernetUDP Udp;// A UDP instance to let us send and receive packets over Ethernet
-
-
 #endif
