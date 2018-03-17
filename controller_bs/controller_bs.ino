cceda4ef074f35ff7f55fea9f707ff768e8e32e2
49V <moshelawlor@gmail.com>
2018-03-17 04:13:41 -0700

    Integrates errory recovery with base station

--- controller_bs/controller_bs.ino
+++ controller_bs/controller_bs.ino
@@ -1,8 +1,10 @@
 #include "constants_bs.h"
 #include "MiniW5100.h"
-#include "SDCard.h"
+#include "SDCard_Base.h"
 
 EthernetClient client; // struct for functions that manipulate communication with server
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -10,6 +12,7 @@ void setup() {
   float sensor_value = 50.0; // REPLACE TEST VALUE
   int capability_id = ID_TEMP; // REPLACE TEST VALUE
   int error = FALSE; // REPLACE TEST VALUE
+  volatile int commFailureOccurred = FALSE;
 
   client = init_ethernet(client);
   print_ip();
@@ -17,6 +20,28 @@ void setup() {
   // successfully-compiled hard code of send_to_server function
   payload = prepare_payload(&payload, capability_id, sensor_value, NULL, &error);
   client = make_http_request(client, payload, &error);
+
+  /* SD interfacing code */
+  int SDerror = FALSE;
+  struct SD_card sd = init_sd(TXT_FILE, &SDerror); // TODO we shouldnt initialize everytime when we refactor this
+
+
+  // We failed to send and SD initialized
+  if(error & !SDerror){
+    write_base_station_message(sd, payload, &SDerror);
+    commFailureOccurred = TRUE;
+  }
+  // We managed to send and SD initialized
+  else if(!error & !SDerror){
+
+  // Check for queued messages
+    if(commFailureOccurred){
+      commFailureOccurred = recover_base_station_data(&sd, client);
+    }
+    
+  }
+  
+
 
   // // Remove from loop and replace x with appropriate sensor values
   // int i;
---
+++ libraries/SDCard/SDCard_Base.h
@@ -0,0 +1,246 @@
+#ifndef SDCARD_H
+#define SDCARD_H
+
+#include "SD.h"
+#define ERROR_VALUE -1000
+#define SENSOR_TXT "Sensor.txt"
+#define BASE_TXT "Base.txt"
+
+struct SD_card {
+  String data_file; // file name to be written to and read from
+  File write_file;
+  File read_file;
+  
+  // Header and tail for keeping track of queue position for reading and writing
+  unsigned long base_station_head = 0;
+  unsigned long base_station_tail = 0;
+  unsigned long sensor_module_head = 0;
+  unsigned long sensor_module_tail = 0;
+};
+
+/*
+ * Purpose: Initialize the SD card and allows for reading and writing
+ * Output: N/A
+ */
+ struct SD_card init_sd(String data_file, int *error) {
+
+   struct SD_card sd;
+   sd.data_file = data_file;
+
+   // Note that even if it's not used as the CS pin, the hardware SS pin
+   // (10 on most Arduino boards, 53 on the Mega) must be left as an output
+   // or the SD library functions will not work.
+   pinMode(PIN_SD, OUTPUT);
+   pinMode(PIN_SD_CHECK, INPUT);
+
+   // check if initialization is complete, and that an SD card is inserted
+   if (SD.begin(PIN_SD) && digitalRead(PIN_SD_CHECK)) {
+    Serial.println(F("Initialization Succeeded"));
+   }
+   else if(!digitalRead(PIN_SD_CHECK)) {
+     Serial.println(ERROR_NOSD);
+     *error = TRUE;
+    }
+
+   else {
+     Serial.println(ERROR_INITSD);
+     *error = TRUE;
+   }
+
+   return sd;
+  }
+
+/*
+ * Purpose: Write to SD file name and return whether or not write was successful
+ * Output: true or false
+ */
+int write_sd(struct SD_card sd, float temperature_data, float humidity_data, float CO2_data, bool airflow_data, int *error) {
+
+  // open file for writing
+  sd.write_file = SD.open(sd.data_file, FILE_WRITE);
+
+  // check that file opened properly
+  if(sd.write_file) {
+
+    sd.write_file.print(F("{ \"id\":"));
+    sd.write_file.print(MODULE_ID);
+    sd.write_file.print(",");
+
+  sd.write_file.print(F("\"temperature\":"));
+  if(temperature_data == ERROR_VALUE) {
+      sd.write_file.print(F("NaN"));
+  }
+  else {
+    sd.write_file.print(temperature_data);
+  }
+  sd.write_file.print(F(","));
+
+  sd.write_file.print(F("\"humidity\":"));
+  if(humidity_data == ERROR_VALUE) {
+    sd.write_file.print(F("NaN"));
+  }
+  else {
+    sd.write_file.print(humidity_data);
+  }
+  sd.write_file.print(F(","));
+
+  sd.write_file.print(F("\"CO2\":"));
+  if(CO2_data == ERROR_VALUE) {
+    sd.write_file.print(F("NaN"));
+  }
+  else{
+      sd.write_file.print(CO2_data);
+  }
+
+   sd.write_file.print(F(","));
+
+  sd.write_file.print(F("\"air flow\":"));
+  if(CO2_data == ERROR_VALUE) {
+    sd.write_file.print(F("NaN"));
+  }
+  else{
+      if (airflow_data == TRUE) {
+        sd.write_file.print(F("Yes"));
+      }
+      else {
+        sd.write_file.print(F("No"));
+      }
+  }
+  sd.write_file.print(F(" }\n"));
+
+  }
+  else {
+  // write unsuccessful
+  *error = TRUE;
+    return 0;
+  }
+
+  sd.write_file.close();
+
+  return 1;
+}
+
+/*
+ * Purpose: Read from SD file name and return whether or not read was successful
+ * Output: true or false
+ */
+int read_sd(struct SD_card sd, int *error){
+
+  //open file for reading
+  sd.read_file = SD.open(sd.data_file, FILE_READ);
+
+  if(sd.read_file) {
+
+    // Read from file until there's nothing else in it
+    while(sd.read_file.available()) {
+    Serial.write(sd.read_file.read());
+  }
+
+  }
+  else {
+    // read unsuccessful
+  *error = TRUE;
+  sd.read_file.close();
+  return 0;
+  }
+
+  sd.read_file.close();
+  return 1;
+}
+
+
+/*
+ * Purpose: Writes base station message to SD Card in the event of an error in transmitting to web server. Increments head pointer.
+ * Output: true or false
+ */ 
+ int write_base_station_message(struct SD_card sd, String unsent_message, int *error){
+
+  // open error file for writing
+  sd.write_file = SD.open(BASE_TXT, FILE_WRITE);
+  
+  // check that file opened properly
+  if(sd.write_file) {
+	  sd.write_file.print(unsent_message);
+	  sd.write_file.print(F("\n"));
+	  
+  }
+  
+  else {
+	  // write unsuccessful
+	*error = TRUE;
+	sd.write_file.close();
+	return 0;
+  }
+
+  sd.write_file.close();
+  return 1;
+ }
+ 
+
+/*
+ * Purpose: Tries to send base station data stored on SD card line by line. This data is only stored in the
+ event of a communication failure. If a line is successfully sent, it will be skipped on the next iteration.
+         
+            if all message are sent,  commFailureOcurred = FALSE; Clear SD card file, and clear header and tail pointer.
+            if any failed, commFailureOccurred = TRUE;
+			
+ * Output: true or false
+ */
+int recover_base_station_data(struct SD_card *sd, EthernetClient client){
+	
+	char line[TX16_REQUEST_MAX_SIZE];
+	int error = FALSE;
+	sd->read_file = SD.open(BASE_TXT, O_RDWR);
+	// As the file is written to, the head will change position
+	sd->sensor_module_head = sd->read_file.position();
+	sd->read_file.seek(sd->sensor_module_tail);
+	
+	if(sd->read_file) {
+		// Read from file until there's nothing else in it
+		while(sd->read_file.available()) {
+
+			// Read the next line
+			sd->read_file.readStringUntil('\n').toCharArray(line, TX16_REQUEST_MAX_SIZE + 1);
+			
+			client = make_http_request(client, line, &error);
+			
+			// Use count < n if you want to simulate values being sent in blocks
+				if(!error){
+					
+					// If the message is successfully sent, we will advance the pointer
+					sd->sensor_module_tail = sd->read_file.position();
+
+				}
+				// Otherwise we had an error transmitting; break and return error	
+				else {
+					Serial.println(F("Error transmitting"));
+					sd->read_file.close();
+					return 1;
+				}
+				
+				// When we run out of data, clear the file
+				if (sd->sensor_module_tail == sd->sensor_module_head)
+				{
+					sd->sensor_module_tail = 0;
+					Serial.println(F("We have reached the end of the file"));
+					sd->read_file.close();
+					sd->write_file = SD.open(BASE_TXT, FILE_WRITE);
+					sd->write_file.truncate(0);
+					sd->write_file.close();
+					return 0;
+				}		
+		}
+	}
+	else {
+		// read unsuccessful
+		Serial.println(F("Didn't open"));
+		sd->read_file.close();
+		return 1;
+	}
+	
+	
+	return 1;
+	
+}
+ 
+#endif
